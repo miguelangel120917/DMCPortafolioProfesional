55d6e0ab79178fda4a60ccda63a35cf3d3b0a1c9
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-06-17 20:57:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from data_analyzer import DataAnalyzer
 
 # ----------------------------------------------------------------------
 # ESTADO DE SESIÓN
