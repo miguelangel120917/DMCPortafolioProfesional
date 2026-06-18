f36a812b78fc6830d48eec88f9577cb4a392cd37
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-06-18 05:04:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,11 @@ else:
     except FileNotFoundError:
         st.error("⚠️ Error técnico: Asegúrate de renombrar tu archivo como 'BankMarketing.csv' en tu repositorio de GitHub.")
     archivo=st.sidebar.file_uploader("Cargue su archivo")
+    if archivo is not None:
+        if archivo.name.endswith(".csv"):
+          datos=pd.read_csv(archivo)
+        elif archivo.name.endswith(".xlsx"):
+          datos=pd.read_excel(archivo)
+        st.write(datos)
+        st.write(datos.info())
+        st.write(datos.describe())
