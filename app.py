750230f1f3bec04a244df4afa0311b04ac65f4da
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-06-18 05:31:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import seaborn as sns
 modulo = st.sidebar.selectbox("Seleccione un Módulo", ["Home","Carga de Dataset"])
 
 if modulo == "Home":
-    st.write("Bienvenido la sesión 1")
     st.title ('Proyecto N°1 de la Especialización de Python')
     st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
     st.markdown(
